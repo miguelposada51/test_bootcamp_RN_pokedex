bf764df5093ce5ef6a2a70a817bb6f7a09f203d8
default-push-user-at-953281969096 <miguelposada51@gmail.com>
2019-07-15 15:29:29 -0500

    add pokemon types functionality

--- app/components/Pokemon.js
+++ app/components/Pokemon.js
@@ -16,32 +16,43 @@ export default class Pokemon extends PureComponent {
           //Have a loading state where when data retrieve returns data.          
           height: '',
           weight: '',
-          types: ''
+          types: '',
+          moves: ''
         }
-       
+    
       }
 
       _signOutAsync = async () => {
-        await AsyncStorage.clear();
+        // await AsyncStorage.clear();
         const { navigation } = this.props;
         navigation.navigate('SignIn');        
       };
 
+      /**
+       * Upload Moves and Types from Pokemon
+       * @param {idPokemon} idPok 
+       */
       async getpokeHeiWei(idPok) {
         try {
-            //const { navigation2 } = this.props; 
             //Assign the promise unresolved first then get the data using the json method. 
-            //const id = navigation2.getParam('url', 'Url image of Pokemon')  
-            const pokemonApiCall = await fetch('https://pokeapi.co/api/v2/pokemon/'+`${idPok.split('/')[6]}`);
+              const pokemonApiCall = await fetch('https://pokeapi.co/api/v2/pokemon/'+`${idPok.split('/')[6]}`);
               const pokemon = await pokemonApiCall.json();              
               this.setState({height: pokemon.height, weight: pokemon.weight });  
-             // Alert.alert('la altura y peso son', `${this.state.height} + ${this.state.weight}` +'use : nothing');
-              
+              //get types availables
+              const arr=[];
+              pokemon.types.map((element) =>{
+                arr.push(element.type.name);
+              }); 
+              this.setState({types: [...arr] });
+              //get moves availables
+            //   const arrMoves=[];
+            //   pokemon.moves.map((element) =>{
+            //     arrMoves.push(element.move.name);
+            //   }); 
+            //   this.setState({moves: [...arrMoves] });                             
         } catch(err) {
             console.log("Error fetching data-----------", err); 
-        }  
-        
-
+        }
     }
 
     render() {
@@ -61,6 +72,7 @@ export default class Pokemon extends PureComponent {
                 <Text style={styles.nameOfPokemon}>Height: {this.state.height}</Text>
                 <Text style={styles.nameOfPokemon}>Weight: {this.state.weight}</Text>
                 <Text style={styles.nameOfPokemon}>Types: {this.state.types}</Text>
+                <Text style={styles.nameOfPokemon}>Moves: {this.state.moves}</Text>
                 <Button title="Sign Out" onPress={this._signOutAsync} />
             </View>
         );
